22a066d9c0d14f28c66e11de3ba63783f624acf3
Jeniffer souza <ajenifferbsouza@gmail.com>
2022-03-31 21:32:05 -0300

    upload

---
+++ main.js
@@ -0,0 +1,97 @@
+var conteudo = document.getElementById("conteudo")
+var clicar = document.getElementById("cx1")
+
+
+clicar.addEventListener("click", function(){
+    var conteudo = document.getElementById("conteudo");
+    if(conteudo.style.display ==="none"){
+        conteudo.style.display = "block";
+    }else{
+        conteudo.style.display ="none";
+    }
+
+});
+$('#cx1').on('click', function(){
+    if($('#frame11').height() == 600){
+         $('#frame11').animate({"height" : "600px"}, 200);
+    }else{
+        $('#frame11').animate({"height" : "600px"}, 200);
+    }
+});
+
+
+
+var clicarPr = document.getElementById("cx2")
+
+clicarPr.addEventListener("click", function(){
+    var conteudo = document.getElementById("conteudo2");
+    if(conteudo.style.display ==="none"){
+        conteudo.style.display = "block";
+    }else{
+        conteudo.style.display ="none";
+    }
+
+});
+$('#cx2').on('click', function(){
+    if($('#frame11').height() == 900){
+         $('#frame11').animate({"height" : "900px"}, 200);
+    }else{
+        $('#frame11').animate({"height" : "900px"}, 200);
+    }
+});
+var clicarTres = document.getElementById("cx3")
+
+clicarTres.addEventListener("click", function(){
+    var conteudo = document.getElementById("conteudo3");
+    if(conteudo.style.display ==="none"){
+        conteudo.style.display = "block";
+    }else{
+        conteudo.style.display ="none";
+    }
+
+});
+$('#cx3').on('click', function(){
+    if($('#frame11').height() == 900){
+         $('#frame11').animate({"height" : "900px"}, 200);
+    }else{
+        $('#frame11').animate({"height" : "900px"}, 200);
+    }
+});
+
+var clicarquatro = document.getElementById("cx4")
+
+clicarquatro.addEventListener("click", function(){
+    var conteudo = document.getElementById("conteudo4");
+    if(conteudo.style.display ==="none"){
+        conteudo.style.display = "block";
+    }else{
+        conteudo.style.display ="none";
+    }
+
+});
+$('#cx4').on('click', function(){
+    if($('#frame11').height() == 900){
+         $('#frame11').animate({"height" : "900px"}, 200);
+    }else{
+        $('#frame11').animate({"height" : "900px"}, 200);
+    }
+});
+
+var clicarcinco = document.getElementById("cx5")
+
+clicarcinco.addEventListener("click", function(){
+    var conteudo = document.getElementById("conteudo5");
+    if(conteudo.style.display ==="none"){
+        conteudo.style.display = "block";
+    }else{
+        conteudo.style.display ="none";
+    }
+
+});
+$('#cx5').on('click', function(){
+    if($('#frame11').height() == 900){
+         $('#frame11').animate({"height" : "900px"}, 200);
+    }else{
+        $('#frame11').animate({"height" : "900px"}, 200);
+    }
+});
